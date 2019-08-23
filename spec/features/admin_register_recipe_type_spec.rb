c9576c9946257f62e185fc1333503ef93bfcc9cd
Joao Barbieri <j.rafaeel@hotmail.com>
2019-08-23 11:09:14 -0300

    Feature: blank recipe type register can not be registered

--- spec/features/admin_register_recipe_type_spec.rb
+++ spec/features/admin_register_recipe_type_spec.rb
@@ -16,4 +16,19 @@ feature 'User register recipe type' do
     expect(page).to have_content('Novo tipo de receita cadastrado com sucesso!')
 
   end
+
+  scenario 'blank recipe type register can not be registered' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Novo Tipo de Receita'
+    fill_in 'Novo Tipo de Receita', with: ''
+    click_on 'Enviar'
+
+    # Assert
+    expect(page).to have_content('Tipo de receita não cadastrado!')
+    expect(page).to have_content('Campo obrigatório!')
+
+  end
 end
